43884208b492f8934ef365fcae963a6b95a21624
Karlo Kalinić <kalini.karlo@gmail.com>
2025-07-11 01:55:06 +0000

    "Stručna Analiza" unutar DODATNO NE PODRŽAVA FONT SLOVO Č ajde nađi najs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Metal+Mania&family=Uncial+Antiqua&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Cinzel+Decorative:wght@400;700;900&family=Uncial+Antiqua&display=swap" rel="stylesheet" />
       </head>
       <body className="font-lato bg-background text-foreground">
         <ConditionalNavbar />
